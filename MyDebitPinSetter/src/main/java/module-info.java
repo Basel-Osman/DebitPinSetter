82564bba217a90e4be624b2cd4d33ad32f16a046
Basel Osman <153784387+Basel-Osman@users.noreply.github.com>
2026-04-12 22:45:45 -0400

    Delete MyDebitPinSetter directory

--- MyDebitPinSetter/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.mydebitpinsetter {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.mydebitpinsetter to javafx.fxml;
-    exports com.example.mydebitpinsetter;
-}
